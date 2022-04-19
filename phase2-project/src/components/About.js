69832d5f35a584ff28ab29f8d91a0cd94066619a
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-19 13:52:26 -0500

    Add some header text

--- phase2-project/src/components/About.js
+++ phase2-project/src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 function About(){
     return(
         <div>
-            <br/>
+            <h1>About Page</h1>
             <h2>This SPA will display some toys and allows the user to add a new one</h2>
         </div>
     );
