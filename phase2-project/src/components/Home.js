806de640edf8d523be890d7b7cd722b71a5a6de1
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-19 11:23:29 -0500

    Add initial content

--- phase2-project/src/components/Home.js
+++ phase2-project/src/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Home(){
-
+    return(
+        <div>
+            <br/>
+            <h1>Welcome to my JSX SPA Project</h1>
+        </div>
+    );
 }
 
 export default Home;
