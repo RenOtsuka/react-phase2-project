7cbca4c04a374cb8f90edcfd50dd8c25cc80561e
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-19 11:29:17 -0500

    Add initial content

--- phase2-project/src/components/About.js
+++ phase2-project/src/components/About.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function About(){
-
+    return(
+        <div>
+            <br/>
+            <h2>This SPA will display some toys and allows the user to add a new one</h2>
+        </div>
+    );
 }
 
 
