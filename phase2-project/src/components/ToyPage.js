3937fe832df0b568838933e97ed9e50f6e4ccce4
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-21 11:52:02 -0500

    Add form component

--- phase2-project/src/components/ToyPage.js
+++ phase2-project/src/components/ToyPage.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from "react";
 import ToyList from "./ToyList"
+import Form from "./Form";
 
 function ToyPage(){
 
     const [toyList, setToyList] = useState([]);
+    const [toggleForm, setToggleForm] = useState(false);
 
     useEffect( () => {
         fetch(`http://localhost:3001/toys`)
@@ -12,9 +14,15 @@ function ToyPage(){
         .catch(error => alert(error));
     },[]);
 
+    function toggle(){
+        setToggleForm( (toggleForm) => !toggleForm);
+    }
+
     return(
         <div>
             <h1>Toy Page</h1>
+            <button onClick={toggle}>Click Here to add a toy</button>
+            {toggleForm ? <Form/> : null}
             <ToyList list={toyList}/>
         </div>
     );
