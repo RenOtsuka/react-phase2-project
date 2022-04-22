157c03b49e48a7fab65a4f484b0d906a8a4b1827
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-22 12:09:42 -0500

    Pass state to appropriate inputs

--- phase2-project/src/components/Form.js
+++ phase2-project/src/components/Form.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Form(){
+function Form({name, img, handleNewName, handleNewImg}){
     return(
         <div>
             <form>
                 <h4>Enter Toy here</h4>
-                <input type="text" name="name" placeholder="Enter name here..."/>
+                <input type="text" name="name" placeholder="Enter name here..." value={name} onChange={handleNewName}/>
                 <br/>
-                <input type="text" name="image" placeholder="Enter image URL here..."/>
+                <input type="text" name="image" placeholder="Enter image URL here..." value={img} onChange={handleNewImg}/>
                 <br/>
                 <input type="submit" name="submit"/>
             </form>
