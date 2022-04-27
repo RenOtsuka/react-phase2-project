4a82aa792f8d2ae55b0eb77423884348aa1eba3e
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-27 10:14:55 -0500

    Add image for styling

--- phase2-project/src/components/About.js
+++ phase2-project/src/components/About.js
@@ -2,9 +2,10 @@ import React from "react";
 
 function About(){
     return(
-        <div>
+        <div className="About">
             <h1>About Page</h1>
             <h2>This SPA will display some toys and allows the user to add a new one</h2>
+            <img src="https://www.pngmart.com/files/3/Toy-Story-Characters-PNG-Image.png" alt="Toy Story Cast"/>
         </div>
     );
 }
