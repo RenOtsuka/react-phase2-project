5fc8a77b91d0535ddc8b95426f17712483f57fcf
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-27 10:19:21 -0500

    Add classname to Home div

--- phase2-project/src/components/Home.js
+++ phase2-project/src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home(){
     return(
-        <div>
+        <div className="Home">
             <h1>Home Page</h1>
             <h2>Welcome to my JSX SPA Project</h2>
         </div>
