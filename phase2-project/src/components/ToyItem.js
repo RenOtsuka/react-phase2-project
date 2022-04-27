fc56e5f132253d85b1ef722a15bd749dc054f2d5
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-27 11:16:34 -0500

    Add functionality to clicking like button

--- phase2-project/src/components/ToyItem.js
+++ phase2-project/src/components/ToyItem.js
@@ -1,15 +1,32 @@
 import React, {useState} from "react";
 
-function ToyItem({name, image, likes}){
+function ToyItem({id, name, image, likes}){
 
     const [likeCount, setLikeCount] = useState(likes);
+
+    function handleLike(){
+        setLikeCount( (likeCount) => likeCount + 1);
+
+        fetch(`http://localhost:3001/toys/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json",
+                "Accept": "applicattion/json"
+            },
+            body: JSON.stringify({
+                likes: likes + 1
+            })
+        })
+        .then(r => r.json())
+        .catch(error => alert(error));
+    }
 
     return(
         <>
         <div className="ToyItem">
             <img src={image} alt={name}/>
             <p>{name}</p>
-            <button>likes: {likes}</button>
+            <button onClick={handleLike}>likes: {likeCount}</button>
         </div>
         <br/>
         </>
