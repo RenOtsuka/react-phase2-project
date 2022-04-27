43f6d9983fd323ae3fe9f2eafb48ea169aed8c3b
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-04-27 10:18:44 -0500

    Add img for styling:

--- phase2-project/src/components/Navbar.js
+++ phase2-project/src/components/Navbar.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 
 function NavBar(){
     return(
-        <div className="navbar">
+        <div className="Navbar">
+            <img src="https://www.pngmart.com/files/12/Toy-Story-PNG-HD.png" alt="ToyStoryLogo"/> 
             <li><Link exact to="/">Home</Link></li>
             <li><Link exact to="/about">About</Link></li>
             <li><Link exact to="/toys">Toys</Link></li>
